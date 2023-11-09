b8d002cdb5033fd6d04cb6442c00b233376a85ea
Dell <mirek.stransky@gmail.com>
2023-11-09 01:18:37 +0100

    tvorba class protivníků

---
+++ src/main/java/cz/czechitas/java2webapps/ukol5/entity/Item.java
@@ -0,0 +1,91 @@
+package cz.czechitas.java2webapps.ukol5.entity;
+
+public class Item {
+    private String name;
+    private String image;
+
+    private String firstStrength;
+    private String secondStrength;
+    private String thirdStrength;
+    private String firstWeakness;
+    private String secondWeakness;
+    private int price;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Item(String name, String image, String firstStrength, String secondStrength, String thirdStrength,
+                String firstWeakness, String secondWeakness, int price) {
+        this.name = name;
+        this.image = image;
+        this.firstStrength = firstStrength;
+        this.secondStrength = secondStrength;
+        this.thirdStrength = thirdStrength;
+        this.firstWeakness = firstWeakness;
+        this.secondWeakness = secondWeakness;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getFirstStrength() {
+        return firstStrength;
+    }
+
+    public void setFirstStrength(String firstStrength) {
+        this.firstStrength = firstStrength;
+    }
+
+    public String getSecondStrength() {
+        return secondStrength;
+    }
+
+    public void setSecondStrength(String secondStrength) {
+        this.secondStrength = secondStrength;
+    }
+
+    public String getThirdStrength() {
+        return thirdStrength;
+    }
+
+    public void setThirdStrength(String thirdStrength) {
+        this.thirdStrength = thirdStrength;
+    }
+
+    public String getFirstWeakness() {
+        return firstWeakness;
+    }
+
+    public void setFirstWeakness(String firstWeakness) {
+        this.firstWeakness = firstWeakness;
+    }
+
+    public String getSecondWeakness() {
+        return secondWeakness;
+    }
+
+    public void setSecondWeakness(String secondWeakness) {
+        this.secondWeakness = secondWeakness;
+    }
+
+
+}
