2f8290a2f1ed5021afdb147e933c92497b36a93d
Dell <mirek.stransky@gmail.com>
2023-11-09 01:19:04 +0100

    tvorba controlleru pro úvodní stránku

--- src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
@@ -1,7 +1,12 @@
 package cz.czechitas.java2webapps.ukol5.controller;
 
+import cz.czechitas.java2webapps.ukol5.entity.Item;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Kontroler obsluhující registraci účastníků dětského tábora.
@@ -9,9 +14,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RegistraceController {
 
+  private int price;
+  private String name;
+  private final List<Item> seznamItem = List.of(
+          new Item("Dart Vader","dart-vader.jpg","Dark side power","Light-saber","Power of telekinesis","Relationship with Luke Skywalker","Moral Conflicts", 250),
+          new Item("Palpatine","palpatine.jpg","Master of the Sith Order","Force lightning","Manipulation and Deception","Overestimation of His Own Power","Vulnerability to Physical Contact", 400),
+          new Item("Dart Maul","darth-maul.jpg","Dark side power","Mastery of Combat Arts","Physical Abilities","Absence of a Reliable Teacher","Overconfidence", 150)
+  );
+
   @GetMapping("/")
-  public String formular() {
-    return "formular";
+  public ModelAndView formular() {
+    ModelAndView modelAndView = new ModelAndView("formular");
+    modelAndView.addObject("item", seznamItem);
+    return modelAndView;
+  }
+
+  @PostMapping("/")
+  public String cena (int price,String name) {
+    this.price = price;
+    this.name = name;
+    return "redirect:/";
   }
 
 }
