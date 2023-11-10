79c1a2c9e2ea26af406b93e7bf4265e4bbf7e910
Dell <mirek.stransky@gmail.com>
2023-11-11 00:24:47 +0100

    tvorba controlleru - metody post - testování validaci

--- src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
@@ -3,17 +3,16 @@ package cz.czechitas.java2webapps.ukol5.controller;
 import cz.czechitas.java2webapps.ukol5.entity.Item;
 import cz.czechitas.java2webapps.ukol5.entity.PersonForm;
 import jakarta.validation.Valid;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-import jakarta.validation.constraints.*;
-
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
+
 
 /**
  * Kontroler obsluhující registraci účastníků dětského tábora.
@@ -21,36 +20,53 @@ import java.util.Scanner;
 @Controller
 public class RegistraceController {
 
-  private String name;
+  private String imageBackground;
   private final Random random = new Random();
   private final List<Item> seznamItem = List.of(
-          new Item("Dart Vader","dart-vader.jpg","Dark side power","Light-saber","Power of telekinesis","Relationship with Luke Skywalker","Moral Conflicts", 250),
+          new Item("Darth Vader","dart-vader.jpg","Dark side power","Light-saber","Power of telekinesis","Relationship with Luke Skywalker","Moral Conflicts", 250),
           new Item("Palpatine","palpatine.jpg","Master of the Sith Order","Force lightning","Manipulation and Deception","Overestimation of His Own Power","Vulnerability to Physical Contact", 400),
-          new Item("Dart Maul","darth-maul.jpg","Dark side power","Mastery of Combat Arts","Physical Abilities","Absence of a Reliable Teacher","Overconfidence", 150)
+          new Item("Darth Maul","darth-maul.jpg","Dark side power","Mastery of Combat Arts","Physical Abilities","Absence of a Reliable Teacher","Overconfidence", 150)
   );
 
   @GetMapping("/")
-  public ModelAndView formular() {
-    ModelAndView modelAndView = new ModelAndView("formular");
-    modelAndView.addObject("item", seznamItem);
+  public ModelAndView index() {
+      ModelAndView modelAndView = new ModelAndView("formular");
+      modelAndView.addObject("item", seznamItem);
+      return modelAndView;
+  }
+  @PostMapping("/")
+  public ModelAndView indexPost(String name){
+
+    if (name.equals("Darth Vader")){
+      imageBackground = "vader-background";
+    } else if (name.equals("Palpatine")) {
+      imageBackground = "palpatine-background";
+    } else if (name.equals("Darth Maul")) {
+      imageBackground = "maul-background";
+    }
+    ModelAndView modelAndView = new ModelAndView("redirect:/registration");
+    modelAndView.addObject("form", new PersonForm());
     return modelAndView;
   }
 
-  @PostMapping("/")
-  public String cena (String name) {
-    this.name = name;
+  @GetMapping("/registration")
+  public ModelAndView indexForm() {
+    ModelAndView modelAndView = new ModelAndView("registration");
+    modelAndView.addObject("form", new PersonForm());
+    modelAndView.addObject("image", imageBackground);
+    return modelAndView;  }
 
-    return "/registration";
-  }
   @PostMapping("/registration")
   public Object form(@Valid @ModelAttribute("form") PersonForm form, BindingResult bindingResult) {
 
     if (bindingResult.hasErrors()) {
-      return "/registration";    }
-
-    return new ModelAndView("/order")
+//      return "registration";
+      return new ModelAndView("registration")
+              .addObject("image", imageBackground);
+    }
+    return new ModelAndView("order")
             .addObject("kod", Math.abs(random.nextInt()))
-            .addObject("email", form.getEmail());
-  }
+            .addObject("person", form);
+  };
 
 }
